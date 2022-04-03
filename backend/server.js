99b9cb40426ceedec09133d1189aaf448898c9f6
Sadman Sakib <ssakib4040@gmail.com>
2022-04-03 16:31:47 +0600

    initial backend

---
+++ backend/server.js
@@ -0,0 +1,8 @@
+const dotenv = require("dotenv").config();
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () =>
+  console.log(`Server is running successfully on port ${PORT}`)
+);
